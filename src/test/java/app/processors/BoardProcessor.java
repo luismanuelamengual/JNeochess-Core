4ce45a99b05baa739901e521a3a59abbc1d7a33a
Luis Amengual <luis.amengual@sitrack.com>
2017-06-28 15:38:58 -0300

    Add usages of san methods

--- src/test/java/app/processors/BoardProcessor.java
+++ src/test/java/app/processors/BoardProcessor.java
@@ -115,63 +115,6 @@ public class BoardProcessor extends ConsoleProcessor {
         console.println();
     }
 
-    private String getSquareString (Square square) {
-
-        File file = square.getFile();
-        Rank rank = square.getRank();
-        StringBuilder squareString = new StringBuilder();
-        switch (file) {
-            case A: squareString.append("a"); break;
-            case B: squareString.append("b"); break;
-            case C: squareString.append("c"); break;
-            case D: squareString.append("d"); break;
-            case E: squareString.append("e"); break;
-            case F: squareString.append("f"); break;
-            case G: squareString.append("g"); break;
-            case H: squareString.append("h"); break;
-        }
-        switch (rank) {
-            case ONE: squareString.append("1"); break;
-            case TWO: squareString.append("2"); break;
-            case THREE: squareString.append("3"); break;
-            case FOUR: squareString.append("4"); break;
-            case FIVE: squareString.append("5"); break;
-            case SIX: squareString.append("6"); break;
-            case SEVEN: squareString.append("7"); break;
-            case EIGHT: squareString.append("8"); break;
-        }
-        return squareString.toString();
-    }
-
-    private Square getSquareFromString (String squareString) {
-
-        char fileChar = squareString.charAt(0);
-        char rankChar = squareString.charAt(1);
-        File file = null;
-        switch (fileChar) {
-            case 'a': file = A; break;
-            case 'b': file = B; break;
-            case 'c': file = C; break;
-            case 'd': file = D; break;
-            case 'e': file = E; break;
-            case 'f': file = F; break;
-            case 'g': file = G; break;
-            case 'h': file = H; break;
-        }
-        Rank rank = null;
-        switch (rankChar) {
-            case '1': rank = ONE; break;
-            case '2': rank = TWO; break;
-            case '3': rank = THREE; break;
-            case '4': rank = FOUR; break;
-            case '5': rank = FIVE; break;
-            case '6': rank = SIX; break;
-            case '7': rank = SEVEN; break;
-            case '8': rank = EIGHT; break;
-        }
-        return Square.getSquare(file, rank);
-    }
-
     private void printBoard (Console console) {
 
         Rank[] ranks = { ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT };
@@ -185,7 +128,7 @@ public class BoardProcessor extends ConsoleProcessor {
             }
             Rank rank = ranks[currentRankIndex];
             console.print(" ");
-            console.print(getRankString(rank));
+            console.print(rank.getSan());
             console.print(" ");
             console.print("║");
             for (int fileIndex = 0; fileIndex <= 7; fileIndex++) {
@@ -201,11 +144,11 @@ public class BoardProcessor extends ConsoleProcessor {
                     console.print(" ");
                     if (pieceSide == WHITE) {
                         console.print(ANSI_YELLOW);
-                        console.print(getPieceString(piece));
+                        console.print(piece.getSan());
                         console.print(ANSI_RESET);
                     } else if (pieceSide == BLACK) {
                         console.print(ANSI_PURPLE);
-                        console.print(getPieceString(piece));
+                        console.print(piece.getSan());
                         console.print(ANSI_RESET);
                     }
                 }
@@ -235,45 +178,9 @@ public class BoardProcessor extends ConsoleProcessor {
             }
             File file = files[currentFileIndex];
             console.print("  ");
-            console.print(getFileString(file));
+            console.print(file.getSan());
             console.print(" ");
         }
         console.println();
-    }
-
-    private String getPieceString (Piece piece) {
-        switch (piece) {
-            case WHITE_PAWN: return "P";
-            case WHITE_KNIGHT: return "N";
-            case WHITE_BISHOP: return "B";
-            case WHITE_ROOK: return "R";
-            case WHITE_QUEEN: return "Q";
-            case WHITE_KING: return "K";
-            case BLACK_PAWN: return "p";
-            case BLACK_KNIGHT: return "n";
-            case BLACK_BISHOP: return "b";
-            case BLACK_ROOK: return "r";
-            case BLACK_QUEEN: return "q";
-            case BLACK_KING: return "k";
-            default: return " ";
-        }
-    }
-
-    private String getFileString (File file) {
-        switch (file) {
-            case A: return "A";
-            case B: return "B";
-            case C: return "C";
-            case D: return "D";
-            case E: return "E";
-            case F: return "F";
-            case G: return "G";
-            case H: return "H";
-            default: return "";
-        }
-    }
-
-    private String getRankString (Rank rank) {
-        return String.valueOf(rank.ordinal()+1);
     }
 }
